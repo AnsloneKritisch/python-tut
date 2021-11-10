2f7aa9da5dc53840d04b0d730cd13c8bcb767a2c
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-10 21:18:34 +0530

    6

---
+++ 01_Basic_Program/02_comment.py
@@ -0,0 +1,10 @@
+# "#" symbol is used for comment a single line.
+"""
+
+This three colon is mainly used for multiline comment.
+
+Lines which Has been Commented either by # or 3colon are not print in output.
+
+"""
+print(" # symbol is used for comment a single line. ")
+print(" 3 colons are used for multiline comment ")
