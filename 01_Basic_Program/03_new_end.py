c70433e100894ad81461e95c38c37478ae6453de
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-10 22:43:35 +0530

    10

--- 01_Basic_Program/03_new_end.py
+++ 01_Basic_Program/03_new_end.py
@@ -1,3 +1,10 @@
-print("Hello \n World")
+# How change into new line without using print statment several times ?
+# By using "\n" we can skip into new line.
+ 
+print(" Hello \n How are You ? ")
+
+# How to delete the gap or add any new word between two print statment ?
+# By using end="you_want_to_add" we can do.
+
 print("Hello", end=" ")
 print("World")
