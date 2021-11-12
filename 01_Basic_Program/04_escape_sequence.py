9fd9a28d679d136db4cd3fd76ebbfc59486bbd5b
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-12 12:20:25 +0530

    13

--- 01_Basic_Program/04_escape_sequence.py
+++ 01_Basic_Program/04_escape_sequence.py
@@ -1,4 +1,8 @@
 # \n and \t are escape sequence character
+#-------------------------------------------
 
 # \n -> print the word or the sentence in new line.
 print("Hello \n world")
+
+# \t -> help to give space between two words or sentence.
+print("Hello \t world")
