1d6ce5e54faf5d7b80a8441f80502913d4a3f1ab
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-13 19:26:52 +0530

    16

--- 01_Basic_Program/05_variable.py
+++ 01_Basic_Program/05_variable.py
@@ -2,3 +2,17 @@
 
 a = "Anshu" # Here a is a variable use for storing a word.
 print(a)
+
+b = 2 #It is integer variable 
+c = 2.5 #it is float function
+
+print("The type of variable a is -> " , type(a))
+print("The type of variable b is -> " , type(b))
+print("The type of variable c is -> " , type(c))
+
+d = "Hello"
+e = "World"
+
+print(d + e) #co
+
+
