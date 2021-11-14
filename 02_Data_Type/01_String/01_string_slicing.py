b2ac9c7567468af4633b9054411f902b2d8b31b1
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-14 19:41:36 +0530

    22

--- 02_Data_Type/01_String/01_string_slicing.py
+++ 02_Data_Type/01_String/01_string_slicing.py
@@ -1,7 +1,9 @@
 a = "I am good"
 print(a)
-# I am good -> Sting ven by the user.
-# 012345678 -> String counted by the Python.
+#  I am good -> Sting ven by the user.
+#  012345678 -> String counted by the Python.
+#-(987654321)-> String counted by the Python but the number are negative.
+
 
 # The Third bracket after a indicates the number of string (a) must be printed.
 print(a[4])
@@ -9,10 +11,9 @@ print(a[4])
 # The third bracket and the semicolumn both determines the range of character that must be printed.
 print(a[0:4])
 
+# If we want to skip any character then we have set it by adding semicolon and input no of character you want to skip .
+print(a[0:9:2])
+
 # If we don't give any range then the range has been set by default full line .
 print(a[:])
 
-# Th
-
-b = len(a)
-print(b)
---
+++ 02_Data_Type/01_String/02_string_function.py
@@ -0,0 +1,13 @@
+# The len() function help to calculate the length of the function.
+b = len(a)
+print(b)
+
+print(a.isalnum()) #alpha numeric means no space
+print(a.isalpha()) #alpha numeric means no space
+print(a.endswith("d")) #alpha numeric means no space
+print(a.count("o")) #count number of charcter
+print(a.capitalize()) #count number of charcter
+print(a.find("am")) #count number of charcter
+print(a.lower()) #count number of charcter
+print(a.upper()) #count number of charcter
+print(a.replace("am","is")) #count number of charcter
