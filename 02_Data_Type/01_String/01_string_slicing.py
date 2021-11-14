62edd24659af8115cbc19c4f010411046d82b5ec
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-14 19:26:41 +0530

    21

--- 02_Data_Type/01_String/01_string_slicing.py
+++ 02_Data_Type/01_String/01_string_slicing.py
@@ -1,2 +1,18 @@
-a = " I am good "
+a = "I am good"
 print(a)
+# I am good -> Sting ven by the user.
+# 012345678 -> String counted by the Python.
+
+# The Third bracket after a indicates the number of string (a) must be printed.
+print(a[4])
+
+# The third bracket and the semicolumn both determines the range of character that must be printed.
+print(a[0:4])
+
+# If we don't give any range then the range has been set by default full line .
+print(a[:])
+
+# Th
+
+b = len(a)
+print(b)
