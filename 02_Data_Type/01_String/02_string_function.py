d269c1726537677adc7e863edc01ad604b6885cb
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-14 19:52:38 +0530

    23

--- 02_Data_Type/01_String/02_string_function.py
+++ 02_Data_Type/01_String/02_string_function.py
@@ -1,13 +1,24 @@
-# The len() function help to calculate the length of the function.
-b = len(a)
-print(b)
+# Important functions of string :-
 
-print(a.isalnum()) #alpha numeric means no space
+# The len() function help to calculate the length of the function.
+a = "The World is big but below my Feet"
+b = len(a)
+print("The length of the string a is -> " , b)
+
+# Alpha numeric means no space(both number and character). string.isalnum() -> helps to determine a alpha numric string
+a = "abcd123"
+print(a.isalnum()) 
+
+# Alpha numeric means no space(only character). string.isalnum() -> helps to determine a alpha numric string
+a = "abcd"
 print(a.isalpha()) #alpha numeric means no space
+
 print(a.endswith("d")) #alpha numeric means no space
-print(a.count("o")) #count number of charcter
+"""print(a.count("o")) #count number of charcter
 print(a.capitalize()) #count number of charcter
 print(a.find("am")) #count number of charcter
 print(a.lower()) #count number of charcter
 print(a.upper()) #count number of charcter
 print(a.replace("am","is")) #count number of charcter
+print(a.replace("am","is")) #count number of charcter
+"""
