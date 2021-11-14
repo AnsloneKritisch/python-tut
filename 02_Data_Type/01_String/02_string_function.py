7a6a6317c4b38608cf4adc3e96274fb139062b9b
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-14 20:09:16 +0530

    24

--- 02_Data_Type/01_String/02_string_function.py
+++ 02_Data_Type/01_String/02_string_function.py
@@ -11,14 +11,33 @@ print(a.isalnum())
 
 # Alpha numeric means no space(only character). string.isalnum() -> helps to determine a alpha numric string
 a = "abcd"
-print(a.isalpha()) #alpha numeric means no space
+print(a.isalpha()) 
 
-print(a.endswith("d")) #alpha numeric means no space
-"""print(a.count("o")) #count number of charcter
-print(a.capitalize()) #count number of charcter
-print(a.find("am")) #count number of charcter
-print(a.lower()) #count number of charcter
-print(a.upper()) #count number of charcter
-print(a.replace("am","is")) #count number of charcter
-print(a.replace("am","is")) #count number of charcter
-"""
+# string.endswith("character") -> helps to find out that the string ends with the given character or not.
+a = "abcd"
+print(a.endswith("d"))
+
+# string.count("character") -> helps to count number of the same charcters are present at the string.
+a = "aaaaaaaaaaaabcd"
+print(a.count("a"))
+
+# string.capitalize() -> helps to convert the first letter of the string into capital.
+a = "anshu"
+print(a.capitalize()) 
+
+# string.find("character") -> helps to check wheather is character is present or not and give its location.
+a = "anshu"
+print(a.find("h")) 
+
+# string.lower() -> convert the all letters of the string into small.
+a = "ANSHU"
+print(a.lower()) 
+
+# string.upper() -> convert the all letters of the string into capital.
+a = "anshu"
+print(a.upper()) 
+
+
+# string.replace("word_string","Word_you_want_to_add") -> helps to replace word.
+a = " He am good."
+print(a.replace("am","is")) 
