fbe1dd881ebd05b41734a991d1dedfb5dbf2c286
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-14 22:57:17 +0530

    31

--- 02_Data_Type/02_List/01_intro_list.py
+++ 02_Data_Type/02_List/01_intro_list.py
@@ -1,19 +1,15 @@
-#We use third
+# List is a kind of string which contains more than one string in it.
+# List is mutable ( It means it can be changed)
+
+# We use third bracket for list and store all the data's in it.
 vowel = [ "a","e","i","o","u"]
 print(vowel)
 
+# We can store both charcters and number in a string.
 no = [1,2,3,4,5]
-print(no)
+print(no) 
 
-
-no[0] = 10
-print(no)
-
-#list is mutable means can change 
-
-
-
-
-
-
-
+#We can add a manually change elements of list. 
+a = [1,2]
+a[0] = 10
+print(a)
