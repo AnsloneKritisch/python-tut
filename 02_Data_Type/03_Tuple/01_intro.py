8929cea0576a3208eee09966de9bb5d10e17ac86
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-15 21:41:34 +0530

    37

--- 02_Data_Type/03_Tuple/01_intro.py
+++ 02_Data_Type/03_Tuple/01_intro.py
@@ -1,4 +1,5 @@
-# Tuple are fix . They are non-mutable.
-#
+# Tuple are fix . They are non-mutable.(It means that it can be changed).
+# small bracket proves that it is a tuple.
+
 no = (1,2,3,4)
 print(no)
