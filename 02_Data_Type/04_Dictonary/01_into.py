551504f1e6f3abde40d99fc1a4704db029566a35
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-16 23:16:22 +0530

    44

--- 02_Data_Type/04_Dictonary/01_into.py
+++ 02_Data_Type/04_Dictonary/01_into.py
@@ -1,17 +1,19 @@
-
-# Dictionay is a key value pair
-#case sensetive
-#KEY ARE IMMUTABLE
+# Dictionay is a key value pair.
+# These are case sensetive
+# In dictionary Keys are IMMUTABLE
+# In dictionary Items are MUTABLE
+# Dictinary are mainy denoted by second bracket {}
 
 d1 = {}
 print(type(d1))
 
+#We can have both integer and string in dictonary.
 a = {"a":1 , "b": 2}
 print(a)
 
 b = {"Rohan" : "fish" ,"Raj" : "Cold Drink"}
 print(b)
 
-# add 
+# add elements through this.
 b["Rajan"] =  "Paneer"
 print(b)
