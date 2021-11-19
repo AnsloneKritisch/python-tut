58dbaad913fcad86010e4593158c752900f14ec6
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-11-19 21:16:43 +0530

    50

--- 03_Project/Project_01/project_01.py
+++ 03_Project/Project_01/project_01.py
@@ -1,12 +1,12 @@
 # Create a dictonary and take input from the user and return the meaning of the word from the dictonary
 # Maximum Four words
-
+print("If you want to know about \n Fish , Cold Drink , Piano , Bat \n ")
 a = { "fish" : "It is an aquatic animal" ,
       "cold drink" : "It is a kind of drink made of soda" ,
       "piano" : "Instrument used for playing music" ,
       "bat" : "It is a kind of wooden instrument used wihile playing game name cricket"}
 
-b = input(" Enter the word : ")
+b = input(" Then please enter the word : ")
 c  = b.lower()
 
 print(" The meaning of the word",b,"is",a[c])
