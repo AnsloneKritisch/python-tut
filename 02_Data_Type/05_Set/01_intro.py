3ad7bc6c7d6c707918ac4a863660e98826c2d7d4
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-12-18 21:51:22 +0530

    set

--- 02_Data_Type/05_Set/01_intro.py
+++ 02_Data_Type/05_Set/01_intro.py
@@ -1,16 +1,11 @@
 # Set is mutable.
-# a is a set.
-
+# Set is the same as mathamatical sets.
+# Sets are mainly are in {} brackets during output.
 a = set([1,2,3])
 print(a)
 print(type(a))
 
-# add
+# add a
 b = set()
 b.add(1)
 print(b)
-
-b.union()
-b.intersection()
-len(b)
-a.remove()
---
+++ 02_Data_Type/05_Set/02_function.py
@@ -0,0 +1,16 @@
+# Set is mutable.
+# a is a set.
+
+a = set([1,2,3])
+print(a)
+print(type(a))
+
+# add
+b = set()
+b.add(1)
+print(b)
+
+b.union()
+b.intersection()
+len(b)
+a.remove()
