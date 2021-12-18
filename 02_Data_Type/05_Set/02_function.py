f4b6bea1217562a962c9f0e716264a5889915232
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-12-18 23:43:00 +0530

    set

--- 02_Data_Type/05_Set/02_function.py
+++ 02_Data_Type/05_Set/02_function.py
@@ -10,11 +10,12 @@ b = set()
 b.add(1)
 print(b)
 
-#
+# helps to find number of elements
 b = set([1,2,3])
 c = len(b)
 print(c)
 
+# helps to remove elements from set
 b = set([1,2,3])
 b.remove(1)
 print(b)
