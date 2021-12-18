dba6c065e09b76d388055b13379db008b3e0b288
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-12-19 00:23:34 +0530

    conditional Statment

---
+++ 01_Basic_Program/07_input.py
@@ -0,0 +1,13 @@
+# To take input from the user we have to use input function
+# After taking the input we need to store it somewhere , so we will store it in variable
+
+#Let "x" be a variable
+
+x = input("Enter What you want to store : ")
+
+print(x) # we will print and check that what have been stored
+
+#If you want to a specific type of data then you can mention it or else it will store all your data as string 
+
+x = int(input("Enter an integer :"))
+print(x)
