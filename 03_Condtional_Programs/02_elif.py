4baf67e85caa448336f18b1811b58c793e5d2322
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-12-19 10:04:39 +0530

    elif

---
+++ 03_Condtional_Programs/02_elif.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number : "))
+b = int(input("Enter another number : "))
+
+if a>b :
+    print(a,"is greater than ",b)
+
+elif a==b :
+    print(a,"is equal to",b)
+
+else :
+    print(b,"is greater than ",a)
