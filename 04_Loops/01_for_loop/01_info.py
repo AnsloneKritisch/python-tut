290d5d9f185e5503ebf56ee1e538b076b0e823d8
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-12-29 17:27:27 +0530

    loop

--- 04_Loops/01_for_loop/01_info.py
+++ 04_Loops/01_for_loop/01_info.py
@@ -1,6 +1,12 @@
 # For - Loop 
-# This is 
+# This is basic format of loop
 
 a = int(input("Enter number of Loops : "))
 for i in range (1 , a+1 ) :
+    print(i)
+
+# We can use 
+
+b = ["a","b","c"]
+for i in b :
     print(i)
