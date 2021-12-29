8582706eaa45f581fd85396cdf1a9684f5da6925
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-12-29 21:38:44 +0530

    loop

--- 04_Loops/01_for_loop/01_info.py
+++ 04_Loops/01_for_loop/01_info.py
@@ -1,12 +1,18 @@
-# For - Loop 
+# For - Loop  
 # This is basic format of loop
 
 a = int(input("Enter number of Loops : "))
 for i in range (1 , a+1 ) :
     print(i)
 
-# We can also use loop for printing all elements of list 
+# We can also use loop for printing all elements of list .
 
 b = ["a","b","c"]
 for i in b :
+    print(i)
+
+# We can also use loop for printing all the items present in dictonary.
+
+c = {"Rohan" : "Maza" ,"Raj" : "Frooti" , "Rajan" : "Pepsi"}
+for i in c.items() :
     print(i)
