5fc814a4cdf11d19a0877f8ee0f3d8cee19ae920
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2021-12-30 20:19:32 +0530

    project

---
+++ 00_Project/Project_04/above_100.py
@@ -0,0 +1,12 @@
+
+
+while(True) :
+    a = int(input("Enter a Number : "))
+    if (a > 100 ):
+        print("Yes your got it right ")
+        break
+    else :
+        print(" Please Try again ")
+        continue
+
+        
