014c9b93f51a118f1ce27a85342e3294d3186c68
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-01-18 10:41:19 +0530

    changes

--- 00_Project/Project_05/guess_game.py
+++ 00_Project/Project_05/guess_game.py
@@ -6,22 +6,26 @@ print(" All The Best ")
 a = 1
 num = 56
 while (a <= 5) :
+    
     b = int (input(" Enter the number : "))
     if (b==num) :
         print("\n Yes you got the right choice ")
         print("\n You are too lucky ")
         print("\n : )  ")
         break
+
     elif(b > num) :
         print("\n Oops ! You choose a larger number ")
-        print("\n You have " , 5-a , " chances left ")
-        
+        print("\n You have " , 5-a , " chances left \n ")
         a = a + 1
         continue
+
     elif(b < num) :
         print("\n Oops ! You choose a smaller number ")
-        print("\n You have " , 5-a , " chances left ")
+        print("\n You have " , 5-a , " chances left \n ")
         a = a + 1
         continue
+
     else :
+        print(" Game Over \n")
         print(" You are unlucky today ")
