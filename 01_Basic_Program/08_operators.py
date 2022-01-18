fd5ddd6938450f74cb345100884a442f2f9c51a5
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-01-18 12:02:49 +0530

    operator

---
+++ 01_Basic_Program/08_operators.py
@@ -0,0 +1,12 @@
+"""
+There are total 6 types of operators :-
+
+1> Assignment Operators
+2> Comparison Operators
+3> Logical Operators
+4> Identity Operators
+5> Membership operators
+6> Bitwise Operators
+
+
+"""
