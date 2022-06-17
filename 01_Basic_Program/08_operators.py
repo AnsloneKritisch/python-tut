80056e4d2273bc5df20b5133388e47ffa392919c
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-06-17 09:23:32 +0530

    new

--- 01_Basic_Program/08_operators.py
+++ 01_Basic_Program/08_operators.py
@@ -2,11 +2,38 @@
 
 There are total 6 types of operators :-
 
-1> Assignment Operators
-2> Comparison Operators
-3> Logical Operators
-4> Identity Operators
-5> Membership operators
-6> Bitwise Operators
+1> Arithmatic Operators
+2> Assignment Operators
+3> Comparison Operators
+4> Logical Operators
+5> Identity Operators
+6> Membership operators
+7> Bitwise Operators
 
 """
+# Arithmatic Operators
+
+print(" Arithmatic Operators \n ")
+print(" 5 + 6 = ", 5+6)
+print(" 6 - 5 = ", 6-5)
+print(" 6 * 5 = ", 5*6)
+print(" 2 ** 3 = ", 2**3)
+print(" 6 / 2 = ", 6/2)
+print(" 6 // 2 = ", 6//2)
+print(" 6 % 2 = ", 6%2)
+
+
+# Assignment Operators
+
+print(" \n Assignment Operators \n ")
+
+x = 10 
+print(x)
+
+x += 7
+print(" x += 7 is equal to ", x )
+
+
+# Comparison Operators
+
+print(" \n Comparison Operators \n ")
