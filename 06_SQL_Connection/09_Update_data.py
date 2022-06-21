1ef605e9de4f7a54cb836ef3f14595a45eae2a16
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-06-21 10:13:25 +0530

    new

---
+++ 06_SQL_Connection/09_Update_data.py
@@ -0,0 +1,29 @@
+try:
+    import mysql.connector
+
+    conn = mysql.connector.connect(host='localhost',user='root',password='anshu',port='3307',database='school')
+
+    if(conn.is_connected()):
+        print("Connection Succed")
+        
+except:
+    print("Connection Failed")
+    
+    
+sql = "UPDATE student set Name= 'Shayam Kumar Das' Fees=1500.6 where srno=3"
+myc = conn.cursor()
+
+try:
+    myc.execute(sql)
+    print("Data Updated Sucessfully")
+    # we need to commit to insert data into table
+    conn.commit()
+    
+
+except:
+    print("Data Update Error")
+    myc.close()
+    
+conn.close()
+
+
