c6ad13cc142f17ddf70a0e5f0b3557e679b5878e
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-06-21 10:08:54 +0530

    new

---
+++ 06_SQL_Connection/08_Delete_Data.py
@@ -0,0 +1,31 @@
+
+
+try:
+    import mysql.connector
+
+    conn = mysql.connector.connect(host='localhost',user='root',password='anshu',port='3307',database='school')
+
+    if(conn.is_connected()):
+        print("Connection Succed")
+        
+except:
+    print("Connection Failed")
+    
+    
+sql = "DELETE from student where srno=3"
+myc = conn.cursor()
+
+try:
+    myc.execute(sql)
+    print("Data Deleted Sucessfully")
+    # we need to commit to insert data into table
+    conn.commit()
+    
+
+except:
+    print("Data delete Error")
+    myc.close()
+    
+conn.close()
+
+
