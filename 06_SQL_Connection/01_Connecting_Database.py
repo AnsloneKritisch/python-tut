b5ecf2d13a3a935cbd9d2d0006606c93d57b9913
AnsloneKritisch <86588354+AnsloneKritisch@users.noreply.github.com>
2022-06-24 16:33:00 +0530

    new

--- 06_SQL_Connection/01_Connecting_Database.py
+++ 06_SQL_Connection/01_Connecting_Database.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-conn = mysql.connector.connect(host='localhost',user='root',password='anshu',port='3306')
+conn = mysql.connector.connect(host='localhost',user='root',password='anshu',port='3307')
 
 if(conn.is_connected()):
     print("Connection Succed")
